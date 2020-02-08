7687eef2bd740dbe7c781721bfe31fa5f4b522d9
Karim Elgazar <karimelgazar565@gmail.com>
2020-02-08 21:56:26 +0200

    add video #22

---
+++ #20_fonts.py
@@ -0,0 +1,57 @@
+
+# ? python -m pip install Pillow
+# ? conda install -c anaconda pillow
+
+from PIL import Image, ImageFont, ImageDraw
+import numpy as np
+import random
+import cv2
+import os
+
+img_w = 1920
+img_h = 1080
+coordination = (400, 300)
+color = (67, 195, 244)  # BGR
+size = 50  # font size
+
+image = np.zeros((img_h, img_w, 3), dtype='uint8')
+
+# Make into PIL Image
+im_p = Image.fromarray(image)
+draw = ImageDraw.Draw(im_p)
+
+# Get a drawing context ==> #? shell:fonts
+
+fonts = [x for x in os.listdir("C:/Windows/Fonts")
+         if x.endswith('.ttf')]
+random.shuffle(fonts)
+
+# ? FOR specific font
+# nirmala_bold = ImageFont.truetype(
+#     "‪C:/Windows/Fonts/BRLNSB.TTF", size)
+
+# draw.text(coordination,
+#           "Allah",
+#           color,
+#           font=nirmala_bold)
+
+x, y = 1, 50
+for index, font in enumerate(fonts[:180]):
+    if (index + 1) % 20 == 0:
+        x += 1
+        y = 50
+
+    draw.text((200 * x, y),
+              "Allah",
+              color,
+              font=ImageFont.truetype(font, size))
+    y += 50
+
+# Convert back to OpenCV image and save
+# BECAUSE ITS TYPE IS :
+# <PIL.Image.Image image mode=RGB size=1000x800 at 0x17E183CC888>
+
+result = np.array(im_p)
+cv2.imshow('image', result)
+# cv2.imwrite('e:/captures/photo.png', result)
+cv2.waitKey(0)
--- README.md
+++ README.md
@@ -227,5 +227,12 @@ If you **don't want to use Anaconda** and already have a Python environment and 
         <a href="https://github.com/karimelgazar/OpenCV-in-Arabic-for-Beginners/blob/master/%2319_waitKey.py">#19_waitKey.py</a>
         </td>
     </tr>
+    <tr>
+        <td>
+        <a href="https://www.youtube.com/watch?v=1dWaUSTT1yE&list=PLO1D3YWS7ep3Pfjls3LjBtp5XdvGpBD6Z" target="_blank">#22 Fonts</a></td>
+        <td>
+        <a href="https://github.com/karimelgazar/OpenCV-in-Arabic-for-Beginners/blob/master/%2320_fonts.py">#20_fonts.py</a>
+        </td>
+    </tr>
 </tbody>
 </table>
